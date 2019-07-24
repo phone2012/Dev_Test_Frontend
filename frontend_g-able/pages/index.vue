40403e247e9d288fa802a4b2c85fded858db2ad3
phone2012 <39938014+phone2012@users.noreply.github.com>
2019-07-24 13:20:54 +0700

    build_new

---
+++ frontend_g-able/pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm10
+      md10
+    >
+
+      <v-card>
+        <v-card-title class="headline">
+          Web Application for Managing Inventory of Products
+        </v-card-title>
+        <v-card-text>
+
+        </v-card-text>
+        <v-card-actions>
+          <v-flex xs12>
+     <v-select
+       :items="items"
+       :menu-props="{ top: true, offsetY: true }"
+       item-text="Branch_Name"
+       label="เลือกสาขา"
+     ></v-select>
+   </v-flex>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            flat
+            nuxt
+          >
+            show
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  data: () => ({
+      items: null,
+      item: null,
+    }),
+  components: {
+    // Logo,
+    // VuetifyLogo
+  },
+  async created() {
+    // console.log('created')
+    //const result = await this.$axios.$get(`/team/hierarchy?team_id=${COOKIES.userInfo.ams.team_id}`);
+    this.item = await this.$http.get(`/branch/find/all`);
+    this.items = this.item.data
+    console.log(this.item.data)
+  }
+}
+</script>
