ff3c034e0d794a9fba7e7f3dabef7a8c45a46435
phone2012 <39938014+phone2012@users.noreply.github.com>
2019-07-24 18:49:52 +0700

    table

--- frontend_g-able/pages/index.vue
+++ frontend_g-able/pages/index.vue
@@ -9,20 +9,22 @@
       sm10
       md10
     >
-
       <v-card>
         <v-card-title class="headline">
           Web Application for Managing Inventory of Products
         </v-card-title>
         <v-card-text>
-
+            {{ branch_id }}
+            {{ datas.data }}
         </v-card-text>
         <v-card-actions>
           <v-flex xs12>
      <v-select
        :items="items"
        :menu-props="{ top: true, offsetY: true }"
+       v-model="branch_id"
        item-text="Branch_Name"
+       item-value="Branch_id"
        label="เลือกสาขา"
      ></v-select>
    </v-flex>
@@ -31,9 +33,19 @@
             color="primary"
             flat
             nuxt
-          >
+           @click="sendValue">
             show
           </v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+
+        </v-card-title>
+        <v-card-actions>
+          <v-flex xs12>
+            <Table/>
+          </v-flex>
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -42,15 +54,18 @@
 
 <script>
 // import Logo from '~/components/Logo.vue'
-// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+ import Table from '~/components/Table_Product.vue'
 
 export default {
   data: () => ({
       items: null,
       item: null,
+      branch_id: null,
+      datas: [],
     }),
   components: {
     // Logo,
+    Table,
     // VuetifyLogo
   },
   async created() {
@@ -59,6 +74,11 @@ export default {
     this.item = await this.$http.get(`/branch/find/all`);
     this.items = this.item.data
     console.log(this.item.data)
-  }
+  },
+  methods: {
+    async sendValue() {
+      this.datas = await this.$http.get(`/collect_product/find/id/${this.branch_id}`);
+    },
+  },
 }
 </script>
